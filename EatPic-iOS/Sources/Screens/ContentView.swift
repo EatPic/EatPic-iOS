4826d390daea4063c6cd274abd093617bec1d225
jwon0523 <jwon010523@gmail.com>
2025-07-11 22:19:30 +0900

    💄 Chore: 디자인 시스템을 위한 폰트 확장 추가

--- EatPic-iOS/Sources/Screens/ContentView.swift
+++ EatPic-iOS/Sources/Screens/ContentView.swift
@@ -4,8 +4,13 @@ public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("One Meal, One Routine")
+                .font(.enRegular(size: 18))
+
+            Text("한 장의 식사, 하나의 루틴")
+                .font(.koRegular(size: 18))
+        }
     }
 }
 
