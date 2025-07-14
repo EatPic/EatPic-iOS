48f7bdfb291c02dd67a4e933e4aa28a56f3ac311
SongCodeMaster <lssyl_@naver.com>
2025-07-14 21:32:13 +0900

    ✨ Feat: 회원가입 뷰에서 쓰일  예시 enum 케이스 작성
    추후에 sibmitLabel 리팩터링 가능성 높습니다.

--- EatPic-iOS/Sources/Common/Enum/SignUpField.swift
+++ EatPic-iOS/Sources/Common/Enum/SignUpField.swift
@@ -6,33 +6,62 @@
 //
 
 import Foundation
-import UIKit
+import SwiftUI
 
 enum SignUpField: CaseIterable, FormFieldType, Hashable {
     
+    /// 아이디 입력 필드
+    case id
     /// 비밀번호 입력 필드
     case password
-
+    
+    
+    /// 텍스트 입력필드 상단 텍스트
     var title: String {
         switch self {
-        case .password: return "비밀번호 입력"
+        case .id:
+            return ""
+        case .password:
+            return "비밀번호 입력"
         }
     }
     
     var placeholder: String {
         switch self {
-        case .password: return "비밀번호를 입력하세요"
+        case .id:
+            return "아이디를 입력하세요"
+        case .password:
+            return "비밀번호를 입력하세요"
         }
     }
-
+    
+    var placeholderFont: Font {
+        return Font.koRegular(size: 17)
+    }
+    
+    var placeholderTextColor: Color {
+        return Color.gray050
+    }
+    
+    /// 보안 입력 여부 (비밀번호면 true)
     var isSecure: Bool {
-        self == .password
+        switch self {
+        case .password:
+            return true
+        default:
+            return false
+        }
     }
-
+    
+    /// 해당 필드에 맞는 키보드 타입 지정
     var keyboardType: UIKeyboardType {
-        self == .default
+        .default
     }
 
+    /// 키보드 asubmit 버튼 스타일 설정
+    /// - '.next': 다음 필드로 이동
+    /// - `.done`: 입력 완료
+    /// - 개발 초기상태에서는 .done으로 임시 고정
     var submitLabel: SubmitLabel {
         .done
     }
