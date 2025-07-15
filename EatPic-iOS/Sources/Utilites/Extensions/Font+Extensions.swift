beb52d554312cf7f503fde38bc0215c9894430ad
jwon0523 <jwon010523@gmail.com>
2025-07-15 20:16:13 +0900

    📝 Docs: 변경된 폰트 스타일에 대한 Swift-Doc 주석 작성

--- EatPic-iOS/Sources/Utilites/Extensions/Font+Extensions.swift
+++ EatPic-iOS/Sources/Utilites/Extensions/Font+Extensions.swift
@@ -6,21 +6,7 @@
 //
 
 import SwiftUI
-/**
- Ag LargeTitle 34pt • 34/Auto
- Ag Title1 28pt • 28/Auto
- Ag Title2 22pt • 22/Auto
- Ag Title3 20pt • 20/Auto
- Ag Headline 17pt • 17/Auto
- Ag Body 17pt • 17/Auto
- Ag Callout 16pt • 16/Auto
- Ag Subhead 15pt • 15/Auto
- Ag Footnote 13pt • 13/Auto
- Ag Caption1 12pt • 12/Auto
- Ag Caption2 11pt • 11/Auto
- Ag bold 15pt • 15/Auto
- Ag bold 13pt • 13/Auto
- */
+
 extension Font {
     enum AppleSDGothicNeo {
         case bold
@@ -36,11 +22,16 @@ extension Font {
         }
     }
     
+    /// AppleSDGothicNeo 폰트를 반환하는 Helper 함수입니다.
+    /// - Parameters:
+    ///   - type: AppleSDGothicNeo의 폰트 종류
+    ///   - size: 폰트 크기 (pt 단위)
+    /// - Returns: 지정된 스타일의 SwiftUI Font 객체
     static func appleSDGothicNeo(type: AppleSDGothicNeo, size: CGFloat) -> Font {
         return .custom(type.value, size: size)
     }
     
-    // MARK: - Typography based on design system (AppleSDGothicNeo)
+    // MARK: - 디자인 시스템 기반한 타이포그래피 (AppleSDGothicNeo)
     
     /// Ag LargeTitle 34pt
     static var dsLargeTitle: Font {
@@ -52,46 +43,57 @@ extension Font {
         appleSDGothicNeo(type: .bold, size: 28)
     }
 
+    /// Ag Title2 22pt
     static var dsTitle2: Font {
         appleSDGothicNeo(type: .bold, size: 22)
     }
 
+    /// Ag Title3 20pt
     static var dsTitle3: Font {
         appleSDGothicNeo(type: .bold, size: 20)
     }
 
+    /// Ag Headline 17pt
     static var dsHeadline: Font {
         appleSDGothicNeo(type: .bold, size: 17)
     }
 
+    /// Ag Body 17pt
     static var dsBody: Font {
         appleSDGothicNeo(type: .regular, size: 17)
     }
 
+    /// Ag Callout 16pt
     static var dsCallout: Font {
         appleSDGothicNeo(type: .regular, size: 16)
     }
 
+    /// Ag Subhead 15pt
     static var dsSubhead: Font {
         appleSDGothicNeo(type: .regular, size: 15)
     }
 
+    /// Ag Footnote 13pt
     static var dsFootnote: Font {
         appleSDGothicNeo(type: .regular, size: 13)
     }
 
+    /// Ag Caption1 12pt
     static var dsCaption1: Font {
         appleSDGothicNeo(type: .regular, size: 12)
     }
 
+    /// Ag Caption2 11pt
     static var dsCaption2: Font {
         appleSDGothicNeo(type: .regular, size: 11)
     }
 
+    /// Ag Bold 15pt
     static var dsBold15: Font {
         appleSDGothicNeo(type: .bold, size: 15)
     }
 
+    /// Ag Bold 13pt
     static var dsBold13: Font {
         appleSDGothicNeo(type: .bold, size: 13)
     }
