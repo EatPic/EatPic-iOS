89b392a449397e2ef024bb9334022e03995ac28c
SongCodeMaster <lssyl_@naver.com>
2025-07-15 15:28:49 +0900

    ✨ Feat: 버튼 공용컴포넌트 width 파라미터 추가
    버튼 UI width 파라미터 추가

--- EatPic-iOS/Sources/Components/PrimaryButton.swift
+++ EatPic-iOS/Sources/Components/PrimaryButton.swift
@@ -24,6 +24,9 @@ struct PrimaryButton: View {
     /// 텍스트 색상
     let textColor: Color
     
+    /// 버튼 너비
+    let width: CGFloat
+    
     /// 버튼 높이
     let height: CGFloat
     
@@ -42,12 +45,14 @@ struct PrimaryButton: View {
     ///   - font: 버튼 내부 텍스트 폰트
     ///   - textColor: 버튼 내부 텍스트 색상
     ///   - height: 버튼 높이
+    ///   - weith:
     ///   - action: 버튼 클릭 시 실행할 동작
     init(
         color: Color,
         text: String,
         font: Font,
         textColor: Color,
+        width: CGFloat,
         height: CGFloat,
         cornerRadius: CGFloat,
         action: @escaping () -> Void
@@ -56,6 +61,7 @@ struct PrimaryButton: View {
         self.text = text
         self.font = font
         self.textColor = textColor
+        self.width = width
         self.height = height
         self.cornerRadius = cornerRadius
         self.action = action
@@ -70,8 +76,7 @@ struct PrimaryButton: View {
                 // 배경: 둥근 사각형 모양
                 RoundedRectangle(cornerRadius: cornerRadius)
                     .fill(color)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: height)
+                    .frame(width: width, height: height)
                 
                 // 버튼 텍스트
                     Text(text)
@@ -88,6 +93,7 @@ struct PrimaryButton: View {
         text: "로그인",
         font: .koRegular(size: 20),
         textColor: Color.white,
+        width: 100,
         height: 50,
         cornerRadius: 10,
         action: {print("로그인하기")})
