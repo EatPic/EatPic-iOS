698898159c47a4f92a1054153e9c0b474fbb7fad
jwon0523 <jwon010523@gmail.com>
2025-07-15 23:51:39 +0900

    ✨ Feat: LocationViewModel 제작

--- EatPic-iOS/Sources/Core/Location/LocationViewModel.swift
+++ EatPic-iOS/Sources/Core/Location/LocationViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import CoreLocation
+
+@Observable
+final class LocationViewModel {
+    var currentLocation: CLLocation?
+    var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    
+    private let locationService: LocationService
+    private var timer: Timer?
+    
+    init(locationService: LocationService = LocationServiceImpl()) {
+        self.locationService = locationService
+    }
+    
+    func start() {
+        locationService.requestAuthorization()
+        locationService.startUpdatingLocation()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { _ in
+            DispatchQueue.main.async {
+                self.currentLocation = self.locationService.currentLocation
+                self.authorizationStatus = self.locationService.authorizationStatus
+            }
+        })
+    }
+    
+    deinit {
+        locationService.stopUpdatingLocation()
+        timer?.invalidate()
+    }
+}
