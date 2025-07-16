9462b51e17e35aec35363b5b5bd980f68147ba7f
jutamin <wonjy0307@naver.com>
2025-07-16 09:30:57 +0900

    ✨ feat: ReactionItemView 생성

---
+++ EatPic-iOS/Sources/Components/ReactionItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ReactionItemView.swift
+//  EatPic-iOS
+//
+//  Created by 원주연 on 7/16/25.
+//
+
+import SwiftUI
+
+struct ReactionItemView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ReactionItemView()
+}
