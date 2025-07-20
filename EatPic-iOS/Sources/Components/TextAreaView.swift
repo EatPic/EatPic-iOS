787bf5e97168a16a4d2d77e871e0cf173fd9cffe
LeeEunJung <125847055+codenameVien@users.noreply.github.com>
2025-07-20 21:17:26 +0900

    ✨[Feat] TextAreaView 컴포넌트 제작 (#44)
    * ✨ Feat: 초기 세팅
    
    Feat: 초기 세팅
    
    * ✨ Feat: TextAreaView
    
    TextAreaView 생성
    
    * ✨ Feat: padding 삭제
    
    padding 삭제
    
    * 🐛 Fix: SwiftLint 해결
    
    SwiftLint 해결
    
    * 🐛 Fix: 오류 수정
    
    오류 수정

---
+++ EatPic-iOS/Sources/Components/TextAreaView.swift
@@ -0,0 +1,81 @@
+//
+//  TextAreaView.swift
+//  EatPic-iOS
+//
+//  Created by 이은정 on 7/17/25.
+//
+
+import SwiftUI
+
+/// 높이는 동적으로 조절 가능한 TextAreaView
+/// - Parameters:
+///   - text: 바인딩된 텍스트 값입니다.
+///   - height: TextArea의 높이입니다.
+///   - backgroundColor: TextArea의 배경 색상입니다.
+///   - borderColor: TextArea의 테두리 색상입니다.
+///   - textColor: 입력되는 텍스트의 색상입니다.
+struct TextAreaView: View {
+    
+    // MARK: - Property
+    /// 바인딩된 텍스트 값
+    @Binding var text: String
+    
+    /// TextArea의 높이
+    let height: CGFloat
+    
+    /// TextArea의 배경 색상
+    let backgroundColor: Color
+    
+    /// TextArea의 테두리 색상
+    let borderColor: Color
+    
+    /// 텍스트의 색상
+    let textColor: Color
+    
+    // MARK: - Init
+    
+    init(
+        text: Binding<String>,
+        height: CGFloat,
+        backgroundColor: Color = .white,
+        borderColor: Color = .gray040,
+        textColor: Color = .gray080
+    ) {
+        self._text = text
+        self.height = height
+        self.backgroundColor = backgroundColor
+        self.borderColor = borderColor
+        self.textColor = textColor
+    }
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack {
+            // 배경과 테두리
+            RoundedRectangle(cornerRadius: 10)
+                .fill(backgroundColor)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(borderColor, lineWidth: 1)
+                )
+            
+            // 실제 텍스트 입력
+            TextEditor(text: $text)
+                .foregroundColor(textColor)
+                .font(.dsBody)
+                .padding(.horizontal, 16)
+                .padding(.vertical, 16)
+        }
+        .frame(height: height)
+    }
+}
+
+#Preview {
+    @Previewable @State var sampleText = "이 레시피는요"
+    
+    TextAreaView(
+        text: $sampleText,
+        height: 140
+    )
+}
