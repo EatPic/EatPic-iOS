57eca7ff5a2dd31cf3c42f9166710e00f99fdf85
codenameVien <ejej2003@naver.com>
2025-07-22 20:26:43 +0900

    ✨ Feat: 초기 파일 생성
    초기 파일 생성

---
+++ EatPic-iOS/Sources/Screens/Home/MainHomeView.swift
@@ -0,0 +1,34 @@
+//
+//  MainHomeView.swift
+//  EatPic-iOS
+//
+//  Created by 이은정 on 7/22/25.
+//
+
+import SwiftUI
+
+struct MainHomeView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Hello, World!")
+                }
+              .customNavigationBar(title: {
+                        HStack {
+                      Circle().frame(width: 32, height: 32)
+                      Text("Title")
+                  }
+              }, right: {
+                  Button {
+                      print("오른쪽 버튼")
+                  } label: {
+                      Image(systemName: "gearshape")
+                  }
+              })
+
+    }
+}
+
+#Preview {
+    MainHomeView()
+}
---
+++ EatPic-iOS/Sources/Screens/Home/TodayMealStatus/MealStatusView.swift
@@ -0,0 +1,22 @@
+//
+//  MealStatusView.swift
+//  EatPic-iOS
+//
+//  Created by 이은정 on 7/22/25.
+//
+
+import SwiftUI
+
+struct MealStatusView: View {
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+}
+
+
+
+#Preview {
+    MealStatusView()
+}
