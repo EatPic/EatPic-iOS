117507fd944c3e17951430001c8e57309932a4f2
jutamin <wonjy0307@naver.com>
2025-07-22 18:55:43 +0900

    ✨ Feat: Navigation 연결
    NavigationRouter에 .community 케이스 추가하여 화면전환연결

--- EatPic-iOS/Sources/Core/Navigation/NavigationRoutingView.swift
+++ EatPic-iOS/Sources/Core/Navigation/NavigationRoutingView.swift
@@ -13,6 +13,7 @@ import SwiftUI
 enum NavigationRoute: Equatable, Hashable {
     case calendar
     case notification
+    case community
 }
 
 /// 화면 전환을 위한 라우팅 처리 전용 View입니다.
@@ -35,6 +36,8 @@ struct NavigationRoutingView: View {
                 EmptyView()
             case .notification:
                 EmptyView()
+            case .community:
+                CommunityMainView()
             }
         }
         .environmentObject(container)
