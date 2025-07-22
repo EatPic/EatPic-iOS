9a825dc1f9f4a37eb796e91dc4bde3c1a18ae54f
LeeEunJung <125847055+codenameVien@users.noreply.github.com>
2025-07-23 07:15:46 +0900

    ✨ [Feat] ModalView 컴포넌트 제작 (#31)
    * ✨ Feat: 모달뷰 구현
    
    Feat: 모달뷰UI 1,2,3 구현
    
    * ✨ 모달뷰 구현
    
    Feat: 모달뷰UI 4 구현
    
    * 📝 Memo: 주석추가
    
    Memo: 모달뷰 1,2,3,4 에 필요한 주석 추가
    
    * ✨ Feat: 모달뷰 5 추가
    
    Feat: 모달뷰5 추가
    
    * 📝 Memo: 주석 수정
    
    주석수정
    
    * 📝 Memo: 주석 수정
    
    주석 수정
    
    * ✨ Feat: 모달뷰5 수정
    
    ProgressBar  불러올수 있게 추가(자물쇠 Badge불러오는 건 추후 수정 필요할듯)
    
    * 🔥 Refactor: 파일명 변경을 위한 파일 삭제 및 생성
    
    파일명 변경
    
    * ✨ Feat: 버튼 공용컴포넌트, swiftlint 수정
    
    버튼 공용컴포넌트, swiftlint 수정
    
    * ✨ Feat: 프리뷰 코드 수정
    
    프리뷰 코드 수정
    
    * ✨ Feat: ModalViewBadge수정
    
    ModalViewBadge수정
    
    * ✨ Feat: Protocol 사용해보기
    
    Protocol 사용해보기
    
    * ✨ Feat: 프로토콜 이용하여 코드 수정
    
    프로토콜 이용하여 코드 수정
    
    * ✨ Feat: 피드백 반영
    
    피드백 반영
    
    * 🐛 Fix : 빈칸 수정
    
    빈칸 수정

---
+++ EatPic-iOS/Sources/Components/AlertModalView.swift
@@ -0,0 +1,103 @@
+//
+//  ModalViewDisabled.swift
+//  EatPic-iOS
+//
+//  Created by 이은정 on 7/14/25.
+
+import SwiftUI
+
+/// 제목 + 설명 + 버튼 으로 이루어진 공통 모달 컴포넌트
+/// - Parameters:
+///   - messageTitle: 모달 제목 메시지의 내용을 담습니다
+///   - messageDescription: 모달 설명 메시지의 내용을 담습니다
+///   - messageColor : 모달 메시지의 색상입니다
+///   - buttonText : 버튼 텍스트입니다
+///   - buttonAction: 버튼 클릭 시 실행할 액션입니다
+struct AlertModalView: View {
+    
+    // MARK: - Property
+    
+    /// 모달 제목 메시지
+    let messageTitle: String
+    
+    /// 모달 설명 메시지
+    let messageDescription: String
+    
+    /// 메시지 색상
+    let messageColor: Color
+    
+    /// 버튼 텍스트
+    let buttonText: String
+    
+    /// 버튼 액션
+    let buttonAction: () -> Void
+    
+    // MARK: - Init
+    init(
+        messageTitle: String,
+        messageDescription: String,
+        messageColor: Color = .black,
+        buttonText: String,
+        buttonAction: @escaping () -> Void
+    ) {
+        self.messageTitle = messageTitle
+        self.messageDescription = messageDescription
+        self.messageColor = messageColor
+        self.buttonText = buttonText
+        self.buttonAction = buttonAction
+    }
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack {
+            /// 모달 아래 어둡게 처리된 배경
+            Color.black.opacity(0.4)
+                .ignoresSafeArea()
+
+            VStack {
+                /// 모달 제목 메시지
+                Text(messageTitle)
+                    .foregroundColor(messageColor)
+                    .font(.dsTitle3)
+                
+                Spacer().frame(height: 36)
+                
+                /// 모달 설명 메시지
+                Text(messageDescription)
+                    .foregroundColor(messageColor)
+                    .font(.dsCallout)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                
+                Spacer().frame(height: 34)
+
+                PrimaryButton(
+                    color: .green060,
+                    text: buttonText,
+                    font: .dsBold15,
+                    textColor: .white,
+                    width: 230,
+                    height: 40,
+                    cornerRadius: 10,
+                    action: buttonAction
+                )
+            }
+            .padding(.top, 20)
+            .padding(.bottom, 16)
+            .frame(width: 270)
+            .background(.white)
+            .cornerRadius(15)
+        }
+    }
+}
+
+#Preview {
+    AlertModalView(
+        messageTitle: "안내",
+        messageDescription: "최대 3개까지 선택 가능합니다.",
+        messageColor: .black,
+        buttonText: "확인buttonText",
+        buttonAction: {
+            print("확인 버튼 눌림")
+        }
+    )
+}
