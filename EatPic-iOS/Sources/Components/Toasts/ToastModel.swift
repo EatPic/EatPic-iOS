bcedd1817393f62e0cc5d6d81c4410dc412f4a76
Mr.Song <lssyl_@naver.com>
2025-07-23 15:01:50 +0900

    ✨[Feat] ToastView 공용 컴포넌트 제작 (#53)
    * 🐛 Bug: 초기화 comma 삭제
    
    * ✨ Feat: ToastView 모델 정의
    
    MVVM구조 설계, Model 정의
    
    * ✨ Feat: 하단에 표시되는 ToastView 제작 중간 커밋
    
    개발시 알수없는 빌드 오류로 인해 중간 커밋 남깁니다.
    
    * ✨ Feat: ToastView ViewModel내부에 화면 띄우는 메서드 작성
    
    이후 커밋은 주석 포함 예정
    
    * 📝 Docs: ToastViewModel 함수에 대한 주석 추가
    
    * ✨ Feat: ToastViewModifier 작성, Modifier 적용된 toast View의 렌더링을 구성
    
    * 🔧 파일위치변경
    
    Screens/Community/ToastViewModel-> Screen/Community/ViewModels/ToastViewModel
    
    * 💄 Design: ToastView width값 조정
    
    maxwidth: .infinity로 관리
    
    * ✨ Feat: HStack Spacer() 삽입하여 디자인 맞춤
    
    * 🔥 Remove: 불필요한 코드 제거
    
    토스트뷰 테스트 용도로 작성된 코드 제거
    
    * ♻️ Refact: SwiftLint 불필요한 옵셔널값 지정 제거
    
    옵셔널 선언한 순간 기본적으로 nil값을 가짐
    
    * ♻️ Refact: 폴더구조 변경
    
    기존위치에서 Components/toasts 파일 내부로 위치 변경
    
    * ♻️ Refact: toastView띄우는 View extension 함수위치  변경
    
    기존위치에서 프로젝트에 쓰이는 뷰 확장 유틸리티 폴더로 변경

---
+++ EatPic-iOS/Sources/Components/Toasts/ToastModel.swift
@@ -0,0 +1,18 @@
+//
+//  ToastModel.swift
+//  EatPic-iOS
+//
+//  Created by 송승윤 on 7/21/25.
+//
+
+import Foundation
+
+/// 토스트 뷰 데이터 모델을 정의하는 구조체
+/// - 화면에 띄울 메시지의 제목 ("title")과 표시 지속 시간(duration)을 명시
+struct ToastModel: Equatable {
+    /// 사용자에게 표시할 토스트 제목
+    let title: String
+    
+    /// 사용자에게 보여질 토스트 지속 시간(초 단위)
+    let duration: TimeInterval
+}
---
+++ EatPic-iOS/Sources/Components/Toasts/ToastViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  ToastViewModel.swift
+//  EatPic-iOS
+//
+//  Created by 송승윤 on 7/21/25.
+//
+
+import Observation
+import Dispatch
+import Foundation
+
+/// `@Observable`을 통해 View에서 상태를 자동 감지하도록 설정된 ViewModel입니다.
+@Observable
+class ToastViewModel {
+    
+    // MARK: - Property
+    
+    /// 현재 화면에 표시될 토스트 정보 (title, duration 포함)
+    var toast: ToastModel?
+    
+    /// 토스트 자동 닫기 예약을 위한 작업 객체
+    private var dismissWorkItem: DispatchWorkItem?
+    
+    // MARK: - Function
+    
+    /// 새로운 토스트를 화면에 띄우는 메서드
+    /// - Parameters:
+    ///   - title: 보여줄 토스트 메시지
+    ///   - duration: 토스트가 화면에 유지될 시간 (초 단위)
+    func showToast(title: String, duration: TimeInterval = 2.0) {
+        // 현재 토스트 상태를 설정하여 화면에 표시되도록 트리거
+        toast = ToastModel(title: title, duration: duration)
+        
+        // 기존 예약된 작업이 있으면 취소 (중복 실행 방지)
+        dismissWorkItem?.cancel()
+        
+        // 새로운 작업을 정의 (토스트 작업 시작점)
+        // - 이 작업은 일정 시간이 지난 후에 'toast = nil'을 실행하여 토스트 닫음
+        let task = DispatchWorkItem { [weak self] in
+            self?.dismissToast()
+        }
+        
+        // 지금 생성한 닫기 작업(task)을 저장
+        dismissWorkItem = task
+        
+        // 일정시간 후('duration = 2.0')에 위에서 만든 작업(task) 실행하도록 예약
+        DispatchQueue.main
+            .asyncAfter(deadline: .now() + duration, execute: task)
+    }
+    
+    /// 현재 토스트뷰 즉시 닫음
+    func dismissToast() {
+        // 상태값을 nil로 변경하여 화면에서 토스트 제거
+        toast = nil
+        
+        // 예약된 작업이 있다면 취소하고 해제
+        dismissWorkItem?.cancel()
+        dismissWorkItem = nil
+    }
+}
