172c21c59340521e7df2b7f454c284c4f61a96a2
SongCodeMaster <lssyl_@naver.com>
2025-07-26 20:13:35 +0900

    ✨ EmailLoginView 파일 추가
    LoginView PR 승인시 폴더링된 파일에 추가할 예정(폴더명 충돌 우려되어 Screen폴더에 임시로 생성)

---
+++ EatPic-iOS/Sources/Screens/EmailLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailLoginView.swift
+//  EatPic-iOS
+//
+//  Created by 송승윤 on 7/26/25.
+//
+
+import SwiftUI
+
+struct EmailLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailLoginView()
+}
