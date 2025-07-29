afb23fc0f32be0afeb283fd7ce8bb23ab7e3128f
jutamin <wonjy0307@naver.com>
2025-07-29 17:56:31 +0900

    ✨ Feat: ExploreMainView UI 1차 구현
    ExploreMainView UI 구현

--- EatPic-iOS/Sources/Screens/Explore/ExploreMainView.swift
+++ EatPic-iOS/Sources/Screens/Explore/ExploreMainView.swift
@@ -8,8 +8,77 @@
 import SwiftUI
 
 struct ExploreMainView: View {
+    @State var searchText: String = ""
+    
+    let columns: [GridItem] = [
+        GridItem(.flexible(minimum: 0), spacing: 9.5),
+        GridItem(.flexible(minimum: 0), spacing: 9.5)
+        ]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            searchBar()
+            exploreFeed()
+        }
+        .padding(.horizontal, 16)
+    }
+    
+    private func searchBar() -> some View {
+        SearchBarView(
+            text: $searchText,
+            placeholder: "계정 또는 해시태그 검색",
+            showsDeleteButton: false,
+            backgroundColor: .white,
+            strokeColor: .gray080,
+            onSubmit: {
+                print("onSubmit")
+            },
+            onChange: {_ in 
+                print("onChange")
+            }
+        )
+    }
+    
+    private func exploreFeed() -> some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 9, content: {
+                ForEach(0..<9) { index in
+                    explorePicCard()
+                }
+            })
+        }
+        .scrollIndicators(.hidden)
+    }
+    
+    private func explorePicCard() -> some View {
+        GeometryReader { geometry in
+            ZStack {
+                Image("Explore/testImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geometry.size.width)
+                    .clipped()
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                HStack(spacing: 4) {
+                    Image("icon_comment")
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                    Text("99+")
+                        .font(.dsBold13)
+                        .foregroundStyle(Color.white)
+                    Image("icon_emotion")
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                    Text("10")
+                        .font(.dsBold13)
+                        .foregroundStyle(Color.white)
+                }
+                .padding(8)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
     }
 }
 
