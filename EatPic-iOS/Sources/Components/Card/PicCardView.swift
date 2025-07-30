dd80fdb8294b96705dc8a11a871b0fc25f127818
Juyeun Won <156973592+jutamin@users.noreply.github.com>
2025-07-30 14:18:05 +0900

    🎨 [Design] CommunityMainView UI 구현 (#58)
    * 🎨 Refactor PicCardView
    
    PicCardView 코드 수정
    
    * 🎨 Refactor PicCardView
    
    PicCardView 파일의 주석과 프리뷰 수정
    
    * ✨ Feat: CommunityMainView UI 1차
    
    CommunityMainView UI 1차 구현
    
    * ✨ Feat: Navigation 연결
    
    NavigationRouter에 .community 케이스 추가하여 화면전환연결
    
    * 🐛 Fix: NavigationFouting 수정
    
    NavigationRouting 코드 수정
    
    * ✨ Feat: CommunityMainView UI 2차 구현
    
    CommunityMainView UI 구현 완료
    
    * ✨ Feat: CommunityMainView UI 최종 수정
    
    CommunityMainView UI 구현 완료
    
    * ✨ Feat: testImage 추가 및 PicCardView 컴포넌트 패딩 오류 수정
    
    testImage 추가 및 PicCardView 컴포넌트 패딩 오류 수정
    
    * ✨ Feat: PicCardView 컴포넌트 비율문제 해결
    
    PicCardView 컴포넌트 비율 문제 해결
    
    * ♻️ Refactor: Model 사용하여 구조 분리
    
    Model 사용하여 구조 분리
    
    * 💄 Style: ellipsis 터치영역 크게 수정
    
    ellipsis 버튼 터치영역 크게 수정

--- EatPic-iOS/Sources/Components/Card/PicCardView.swift
+++ EatPic-iOS/Sources/Components/Card/PicCardView.swift
@@ -11,16 +11,16 @@ import SwiftUI
 ///   - profileImage: 프로필 이미지
 ///   - profileID: 프로필 아이디
 ///   - time: 픽카드 업로드 시간
-///   - onEllipsisTapped: 메뉴버튼(eclipsis) 클릭시 실행할 action
+///   - menuContent: 메뉴버튼(eclipsis) 클릭 시 나타날 버튼목록 커스텀
 ///   - postImage: 업로드 이미지
 ///   - myMemo: 사용자가 작성하는 '나의 메모'
-struct PicCardView: View {
+struct PicCardView<Content: View>: View {
     
     // MARK: property
     let profileImage: Image
     let profileID: String
     let time: String
-    let onEllipsisTapped: () -> Void
+    let menuContent: () -> Content
     let postImage: Image
     let myMemo: String
     
@@ -29,21 +29,21 @@ struct PicCardView: View {
         profileImage: Image,
         profileID: String,
         time: String,
-        onEllipsisTapped: @escaping () -> Void,
+        @ViewBuilder menuContent: @escaping () -> Content,
         postImage: Image,
         myMemo: String
     ) {
         self.profileImage = profileImage
         self.profileID = profileID
         self.time = time
-        self.onEllipsisTapped = onEllipsisTapped
+        self.menuContent = menuContent
         self.postImage = postImage
         self.myMemo = myMemo
     }
     
     // MARK: body
     var body: some View {
-        VStack(spacing: 16) {
+        VStack(alignment: .leading, spacing: 16) {
             // 카드 상단 업로드 정보(프로필, 시간)
             HStack {
                 profileImage
@@ -62,32 +62,48 @@ struct PicCardView: View {
                 
                 Spacer()
                 
-                Button(action: {
-                    onEllipsisTapped()
-                }, label: {
+                Menu {
+                    menuContent()
+                } label: {
                     Image(systemName: "ellipsis")
-                        .frame(width: 24, height: 24)
+                        .resizable()
+                        .frame(width: 24, height: 5)
                         .foregroundStyle(Color.black)
-                })
+                        .padding(.trailing, 2)
+                        .padding([.leading, .top, .bottom], 16) // 실제 터치 영역 넓히기
+                        .contentShape(Rectangle()) // 패딩 영역까지 터치 인식
+                }
             }
-                        
+            
             // 업로드 이미지 (정사각형 + 모서리 둥글게)
             // 이미지의 사이즈는 기기의 화면 너비에 따라 달라지도록 설정 (화면 너비를 꽉채우도록)
-            let screenWidth = UIScreen.main.bounds.width
-            postImage
-                .resizable()
-                .scaledToFill()
-                .frame(maxWidth: .infinity)
-                .aspectRatio(1, contentMode: .fit) // 정사각형 유지
-                .clipped()
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            
+            GeometryReader { geometry in
+                ZStack {
+                    postImage
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: geometry.size.width)
+                        .clipped()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    PicCardItemView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity,
+                               alignment: .bottomLeading)
+                }
+            }
+            .aspectRatio(1, contentMode: .fit)
+
             // 사용자 메모 (나의 메모)
             Text(myMemo)
                 .font(.dsSubhead)
                 .foregroundColor(.gray080)
+                .frame(alignment: .leading)
         }
     }
+}
+
+#Preview {
+    CommunityMainView()
 }
 
 #Preview {
@@ -95,8 +111,27 @@ struct PicCardView: View {
         profileImage: Image(systemName: "circle.fill"),
         profileID: "아이디",
         time: "오후 6:29",
-        onEllipsisTapped: { print("메뉴 선택") },
-        postImage: Image(systemName: "square.fill"),
+        menuContent: {
+            Button(action: {
+                print("사진 앱에 저장")
+            }) {
+                Label("사진 앱에 저장", systemImage: "square.and.arrow.down")
+            }
+            
+            Button(action: {
+                print("수정하기")
+            }) {
+                Label("수정하기", systemImage: "square.and.pencil")
+            }
+            
+            // role을 destructive로 설정 시, 빨간 버튼으로 만들 수 있음
+            Button(role: .destructive, action: {
+                print("삭제하기")
+            }) {
+                Label("삭제하기", systemImage: "exclamationmark.bubble")
+            }
+        },
+        postImage: Image("Community/testImage"),
         myMemo: "오늘은 샐러드를 먹었습니다~ 아보카도를 많이 넣어 먹었어요~~~~~~다들 제 레시피 보고 따라 만들어주시기길......태그도 남겨주시기르를"
     )
 }
