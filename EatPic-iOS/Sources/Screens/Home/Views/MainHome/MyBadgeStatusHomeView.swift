d8c9e25cec009ead4a990c7a3f02ccfa9d3afa00
codenameVien <ejej2003@naver.com>
2025-08-01 11:50:26 +0900

    ✨ 뱃지홈스크롤 뷰 수정

--- EatPic-iOS/Sources/Screens/Home/Views/MainHome/MyBadgeStatusHomeView.swift
+++ EatPic-iOS/Sources/Screens/Home/Views/MainHome/MyBadgeStatusHomeView.swift
@@ -13,43 +13,52 @@ struct MyBadgeStatusHomeView: View {
     
     var body: some View {
         VStack {
-            HStack {
-                Text("나의 뱃지 현황")
-                    .font(.dsTitle3)
-                    .foregroundColor(.gray080)
-                
-                Spacer()
-                
-                Button(action: {
-                    container.router.push(.myBadgeStatusAll)
-                    print("전체보기")
-                }, label: {
-                    Text("전체보기 >")
-                        .foregroundStyle(Color.green060)
-                        .font(.dsSubhead)
-                })
-            }
+            titleBar
             
-            // 뱃지 리스트
-            ScrollView(.horizontal, showsIndicators: false) {
-                LazyHStack(spacing: 8) {
-                    
-                    ForEach(viewModel.badgeItems) { badgeItem in
-                        BadgeView(
-                            state: badgeItem.state,
-                            badgeName: badgeItem.name
-                        )
-                        .scaleEffect(0.77)
-                        .frame(width: 100)
-                    }
-                }
-            }
+            badgeScroll
         }
         .padding(.top, 16)
         .padding(.horizontal, 19)
         .frame(height: 202)
         .background(.white)
         .cornerRadius(15)
+    }
+    
+    // MARK: 해당 뷰의 상단 제목 바
+    private var titleBar: some View {
+        HStack {
+            Text("나의 뱃지 현황")
+                .font(.dsTitle3)
+                .foregroundColor(.gray080)
+            
+            Spacer()
+            
+            Button(action: {
+                container.router.push(.myBadgeStatusAll)
+                print("전체보기")
+            }, label: {
+                Text("전체보기 >")
+                    .foregroundStyle(Color.green060)
+                    .font(.dsSubhead)
+            })
+        }
+    }
+
+    // MARK: 뱃지 스크롤 뷰
+    private var badgeScroll: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHStack(spacing: 8) {
+                
+                ForEach(viewModel.badgeItems) { badgeItem in
+                    BadgeView(
+                        state: badgeItem.state,
+                        badgeName: badgeItem.name
+                    )
+                    .scaleEffect(0.77)
+                    .frame(width: 100)
+                }
+            }
+        }
     }
 }
 
