6211a778ffa69195400cc35a66521d8ab14f025b
SongCodeMaster <lssyl_@naver.com>
2025-08-01 17:15:39 +0900

    ♻️ Refact: deprecated 제거

--- EatPic-iOS/Sources/Components/Card/ReciptDetailCardView.swift
+++ EatPic-iOS/Sources/Components/Card/ReciptDetailCardView.swift
@@ -118,7 +118,10 @@ struct RecipeDetailCardView: View {
                 } label: {
                     Image("icon_link")
                         .padding(5)
-                        .background(Circle().fill(Color.white))
+                        .background(alignment: .center) {Circle().fill(
+                            Color.white
+                        )
+                        }
                 }
             }
             
@@ -134,7 +137,8 @@ struct RecipeDetailCardView: View {
                     }
                     .padding(.vertical, 5)
                     .padding(.horizontal, 8)
-                    .background(Color.white.cornerRadius(50))
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
                 }
             }
         }
