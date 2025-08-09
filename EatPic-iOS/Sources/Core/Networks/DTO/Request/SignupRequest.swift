3a9698ddd684a25c99a9252159be52e67ec92108
SongCodeMaster <lssyl_@naver.com>
2025-08-09 18:09:05 +0900

    ✨ Feat: 회원가입 API 요청 구조체 작성

---
+++ EatPic-iOS/Sources/Core/Networks/DTO/Request/SignupRequest.swift
@@ -0,0 +1,22 @@
+//
+//  SignupRequest.swift
+//  EatPic-iOS
+//
+//  Created by 송승윤 on 8/9/25.
+
+import Foundation
+
+/// 회원가입 API 요청 구조체
+struct SignupRequest: Codable {
+    let role: String
+    let email: String
+    let password: String
+    let passwordConfirm: String
+    let nameId: String
+    let nickname: String
+    let termsAgreed: String
+    let privacyPolicyAgreed: String
+    let marketingAgreed: String
+    let notificationAgreed: String
+    let passwordMatching: String
+}
