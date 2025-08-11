0778dbeaeca225106b9449b318a4ba702b433a90
SongCodeMaster <lssyl_@naver.com>
2025-08-11 14:40:08 +0900

    ✨ Feat: 이메일로  회원가입 API targetType 작성

--- EatPic-iOS/Sources/Core/Networks/TargetType/Auth/AuthTargetType.swift
+++ EatPic-iOS/Sources/Core/Networks/TargetType/Auth/AuthTargetType.swift
@@ -11,34 +11,47 @@ import Moya
 /// 로그인 등의 인증 요청을 위한 API를 정의하는 TargetType입니다.
 enum AuthTargetType {
     case login(email: String, password: String)
+    case signup(request: SignupRequest)
 }
 
 extension AuthTargetType: APITargetType {
     var path: String {
-        return "/auth/login/social"
+        switch self {
+        case .login:
+            return "/auth/login/social"
+        case .signup:
+            return "/api/auth/signup"
+        }
+
     }
 
     var method: Moya.Method {
-        return .post
+        switch self {
+        case .login, .signup:
+            return .post
+        }
     }
 
     var task: Task {
         switch self {
         case let .login(email, password):
             let parameters = ["email": email, "password": password]
-            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+            return .requestParameters(
+                parameters: parameters,
+                encoding: JSONEncoding.default
+            )
+        case .signup(let request):
+            return .requestJSONEncodable(request)
         }
     }
     
     var sampleData: Data {
-        switch self {
-        case .login:
-            return Data("""
-            {
-                "user_id": 1,
-                "token": "jwt-token"
-            }
-            """.utf8)
+        return Data("""
+        {
+            "user_id": 1,
+            "token": "jwt-token"
         }
+        """.utf8)
     }
 }
+
