2b33fd887c58f4344aee93d194fb5a98c7a7546d
codenameVien <ejej2003@naver.com>
2025-08-15 16:22:34 +0900

    ✨ Feat: NavigationBar 적용

--- EatPic-iOS/Sources/Screens/MyPage/AllPicCard/AllPicCardView.swift
+++ EatPic-iOS/Sources/Screens/MyPage/AllPicCard/AllPicCardView.swift
@@ -10,20 +10,35 @@ import SwiftUI
 struct AllPicCardView: View {
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: [
-                GridItem(.flexible(), spacing: 8),
-                GridItem(.flexible(), spacing: 8),
-                GridItem(.flexible(), spacing: 8)
-            ], spacing: 8) {
-                ForEach(0..<30, id: \.self) { _ in
-                    Rectangle()
-                        .fill(Color.gray020)
-                        .frame(width: 118, height: 118)
+        
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [
+                    GridItem(.flexible(), spacing: 8),
+                    GridItem(.flexible(), spacing: 8),
+                    GridItem(.flexible(), spacing: 8)
+                ], spacing: 8) {
+                    ForEach(0..<30, id: \.self) { _ in
+                        Rectangle()
+                            .fill(Color.gray020)
+                            .frame(width: 118, height: 118)
+                    }
                 }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 8)
             }
-            .padding(.horizontal, 16)
-            .padding(.vertical, 8)
+        }
+        .customNavigationBar {
+            Text("차단된 계정")
+        } right: {
+            Button(action: {
+                // TODO: 필터 버튼 액션 구현
+            }, label: {
+                Image("btn_mypage_filter")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24, height: 24)
+            })
         }
     }
 }
