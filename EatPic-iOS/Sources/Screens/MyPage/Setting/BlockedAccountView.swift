14a50f7c9582182dd69beef048f7e1c7b476ec41
codenameVien <ejej2003@naver.com>
2025-08-15 20:12:43 +0900

    ✨ Feat: BlockedAccount에 Modal 띄우게 추가

--- EatPic-iOS/Sources/Screens/MyPage/Setting/BlockedAccountView.swift
+++ EatPic-iOS/Sources/Screens/MyPage/Setting/BlockedAccountView.swift
@@ -8,24 +8,48 @@
 import SwiftUI
 
 struct BlockedAccountView: View {
+    @State private var showUnblockModal = false
+    @State private var selectedNickname = "닉네임"
+    @State private var selectedId = "아이디"
+    
     var body: some View {
-        
-        ScrollView {
-            VStack(spacing: 0) {
-                ForEach(0..<3, id: \.self) { _ in
-                    blockedAccount
+        ZStack {
+            ScrollView {
+                VStack(spacing: 0) {
+                    ForEach(0..<3, id: \.self) { index in
+                        blockedAccount(index: index)
+                    }
                 }
             }
-        }
-        .customNavigationBar {
-            Text("차단된 계정")
-        } right: {
-            EmptyView()
+            .customNavigationBar {
+                Text("차단된 계정")
+            } right: {
+                EmptyView()
+            }
+            
+            // 차단 해제 모달
+            if showUnblockModal {
+                DecisionModalView(
+                    message: "\(selectedNickname)(@\(selectedId))님을 차단 해제하시겠어요?",
+                    messageColor: .gray080,
+                    leftBtnText: "아니오",
+                    rightBtnText: "차단 해제",
+                    rightBtnColor: .red050,
+                    leftBtnAction: {
+                        showUnblockModal = false
+                        print("아니오 버튼 클릭, 모달 닫기 액션")
+                    },
+                    rightBtnAction: {
+                        showUnblockModal = false
+                        print("차단 해제 버튼 클릭, 모달닫기 액션")
+                    }
+                )
+            }
         }
     }
     
     // MARK: 차단된 계정
-    private var blockedAccount: some View {
+    private func blockedAccount(index: Int) -> some View {
         HStack(spacing: 12) {
             Circle()
                 .fill(Color.gray040)
@@ -43,7 +67,9 @@ struct BlockedAccountView: View {
             Spacer()
             
             Button("차단 해제") {
-                print("차단 해제")
+                selectedNickname = "닉네임"
+                selectedId = "아이디"
+                showUnblockModal = true
             }
             .font(.dsBold13)
             .foregroundStyle(Color.gray050)
