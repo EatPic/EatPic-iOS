14e4b48838996f5d7eb44223cf3a6a7771a88ca6
jutamin <wonjy0307@naver.com>
2025-08-19 01:54:51 +0900

    ✨ Feat: 나의 아이콘 api 연결을 위한 response 구조체 작성

--- EatPic-iOS/Sources/Core/Networks/DTO/Response/UserIconResponse.swift
+++ EatPic-iOS/Sources/Core/Networks/DTO/Response/UserIconResponse.swift
@@ -1,5 +1,5 @@
 //
-//  FollowingUserIconResponse.swift
+//  UserIconResponse.swift
 //  EatPic-iOS
 //
 //  Created by 원주연 on 8/19/25.
@@ -26,5 +26,17 @@ struct UserIcon: Codable, Identifiable {
     let isFollowing: Bool?
 
     // SwiftUI ForEach 등에서 사용하기 위해 id 프로퍼티 추가
+    var id: Int { userId }
+}
+
+// 나의 아이콘을 불러오기 위한 응답구조체
+struct MyIcon: Codable, Identifiable {
+    let userId: Int
+    let profileImageUrl: String
+    let nameId: String
+    let nickname: String
+    let introduce: String
+    let isFollowing: Bool
+    
     var id: Int { userId }
 }
