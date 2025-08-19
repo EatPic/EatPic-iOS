c0ba7c10148dfdf8f02a8dacb9ef38082b53f863
jutamin <wonjy0307@naver.com>
2025-08-19 13:48:57 +0900

    ✨ Feat: FeedReponse, IconResponse 구조체 수정 및 구현

--- EatPic-iOS/Sources/Core/Networks/DTO/Response/UserIconResponse.swift
+++ EatPic-iOS/Sources/Core/Networks/DTO/Response/UserIconResponse.swift
@@ -7,17 +7,18 @@
 
 import Foundation
 
-// Communtiy탭 상단 팔로잉하는 유저 아이콘을 띄우기 위한 응답구조체
-// result 영역
-struct UserIconResult: Codable {
+// Communtiy탭 상단 팔로잉하는 유저 아이콘을 띄우기 위한 응답구조체 (팔로잉하는 유저들, 나)
+
+// 팔로잉하는 유저들을 불러오기 위한 응답구조체
+struct UserListResult: Codable {
     let page: Int
     let size: Int
     let total: Int
-    let userIconList: [UserIcon]
+    let userIconList: [UserIconDTO]
 }
 
-// 실제 유저 정보
-struct UserIcon: Codable, Identifiable {
+// 팔로잉하는 유저들 정보
+struct UserIconDTO: Codable, Identifiable {
     let userId: Int
     let profileImageUrl: String
     let nameId: String
@@ -29,14 +30,42 @@ struct UserIcon: Codable, Identifiable {
     var id: Int { userId }
 }
 
+// API -> UserIcon 변환로직
+extension UserIconDTO {
+    func toCommunityUser() -> CommunityUser {
+        CommunityUser(
+            id: userId,
+            nameId: nameId,
+            nickname: nickname,
+            imageName: profileImageUrl
+        )
+    }
+}
+
 // 나의 아이콘을 불러오기 위한 응답구조체
-struct MyIcon: Codable, Identifiable {
+struct MyUserIconResult: Codable, Identifiable {
     let userId: Int
-    let profileImageUrl: String
+    let profileImageUrl: String?
     let nameId: String
     let nickname: String
-    let introduce: String
-    let isFollowing: Bool
+    let introduce: String?
+    let isFollowing: Bool?
     
     var id: Int { userId }
+}
+
+// API -> UserIcon 변환로직
+extension MyUserIconResult {
+    func toCommunityUser() -> CommunityUser {
+        CommunityUser(
+            id: userId,
+            nameId: nameId,
+            nickname: nickname,
+            imageName: profileImageUrl,
+            introduce: introduce,
+            type: .me,
+            isCurrentUser: true,
+            isFollowed: false
+        )
+    }
 }
