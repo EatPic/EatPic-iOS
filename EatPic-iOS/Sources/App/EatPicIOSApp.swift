c5904bca9914476f58570ba1c9215f27e4155469
jwon0523 <jwon010523@gmail.com>
2025-08-20 20:54:27 +0900

    ✨ Feat: 자동 로그인 & 로그아웃 기능 추가

--- EatPic-iOS/Sources/App/EatPicIOSApp.swift
+++ EatPic-iOS/Sources/App/EatPicIOSApp.swift
@@ -5,6 +5,7 @@ struct EatPicIOSApp: App {
     /// 앱 흐름 상태 뷰모델
     @StateObject private var appFlowViewModel: AppFlowViewModel = .init()
     @StateObject private var container: DIContainer = .init()
+    @State private var didBootstrap = false
     
     init() {
         GlobalNavigationBarStyle.apply()
@@ -12,14 +13,29 @@ struct EatPicIOSApp: App {
     
     var body: some Scene {
         WindowGroup {
-            switch appFlowViewModel.appState {
-            case .login:
-                LoginView(container: container, appFlowViewModel: appFlowViewModel)
-            case .tab:
-                MainTabView(container: container)
-                    .task {
-                        container.locationStore.start()
-                    }
+            Group {
+                switch appFlowViewModel.appState {
+                case .login:
+                    LoginView(container: container, appFlowViewModel: appFlowViewModel)
+                case .tab:
+                    MainTabView(container: container)
+                        .task {
+                            container.locationStore.start()
+                        }
+                }
+            }
+            .task {
+                // 앱 시작 후 최초 1회만 JWT 존재 여부 체크
+                guard !didBootstrap else { return }
+                didBootstrap = true
+                
+                let userSession = container.userSessionKeychain.loadSession(for: .userSession)
+                let accessToken = userSession?.accessToken
+                let hasValid = accessToken?.isEmpty == false
+                
+                await MainActor.run {
+                    appFlowViewModel.appState = hasValid ? .tab : .login
+                }
             }
         }
         .environmentObject(appFlowViewModel)
