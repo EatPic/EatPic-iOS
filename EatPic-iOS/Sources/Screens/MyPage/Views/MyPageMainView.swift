c8e590cfe8b03ea222b9a1f63f3a469f0b6ed8a0
codenameVien <ejej2003@naver.com>
2025-08-22 05:26:30 +0900

    ✨ Feat: 코드 수정

--- EatPic-iOS/Sources/Screens/MyPage/Views/MyPageMainView.swift
+++ EatPic-iOS/Sources/Screens/MyPage/Views/MyPageMainView.swift
@@ -105,7 +105,7 @@ struct MyPageMainView: View {
     /// 마이페이지 프로필 사용자 한줄소개 텍스트
     private var middleIntroduction: some View {
         VStack {
-            Text(viewModel.user?.introduce ?? "안녕하세요 밥 먹는거 좋아하는 잇콩입니다\n밥을 정말 좋아합니다.")
+            Text(viewModel.user?.introduce ?? "")
                 .fixedSize(horizontal: false, vertical: true)
         }
         .font(.dsCaption1)
